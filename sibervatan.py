ff999d455f2de590e918809b9e860ef2df9c9a44
Efekaanu <155834429+Efekaanu@users.noreply.github.com>
2024-01-10 20:43:16 +0300

    Add files via upload

---
+++ sibervatan.py
@@ -0,0 +1,21 @@
+print("Hello")
+print(5**5)
+sayi=-8
+print("mutlak deger",abs(sayi))
+us=pow(2,3)
+print("sayi ussu",us)
+from math import sqrt
+sayi=25
+karekok=sqrt(sayi)
+print("sayinin karekoku",karekok)
+
+
+from math import ceil,floor
+ondalik=10.25
+yukari=ceil(ondalik)
+asagi=floor(ondalik)
+print("yukari",yukari)
+print("asagi", asagi)
+
+print(5*5)
+print(500+10)
