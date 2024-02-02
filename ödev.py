3249ee20060dfb1420bad4e6406cf7c831a0371a
Efekaanu <155834429+Efekaanu@users.noreply.github.com>
2024-02-02 22:06:09 +0300

    ödev

---
+++ ödev.py
@@ -0,0 +1,48 @@
+tuple = (10,20,30)
+print(tuple)
+print(tuple[1])
+
+karşılama=("Efe Kaan Karaman")
+karşılama_lower =  karşılama.lower()
+print(karşılama_lower)
+
+en_büyük_sayı=max(karşılama)
+print(karşılama)
+
+karşılama_upper =  karşılama.upper()
+print(karşılama_upper)
+
+print (karşılama[1:14])
+
+tuple = (10,20,30)
+print(tuple)
+print(tuple[1])
+
+dict={'anahtar1':'değer1','anahtar2':'değer2'}
+print(dict)
+değer=dict['anahtar1']
+print(değer)
+
+
+
+uzunluk = len(karşılama)
+print(karşılama[-1])
+
+karşılama_endswith = karşılama.endswith("e")
+print(karşılama_endswith)
+
+karşılama_replace = karşılama.replace('a','@')
+print(karşılama_replace)
+
+karşılama_join ="0".join(karşılama)
+print(karşılama_join)
+
+karşılama_find = karşılama.find("aa")
+print(karşılama_find)
+
+karşılama.reverse()
+print(karşılama)
+
+sayılar=[10,8,5,3,15]
+sayılar.reverse()
+print(sayılar)
