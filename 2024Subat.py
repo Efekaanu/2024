c3631495835be4feb39a7bbef8a0fe16d86f2667
Efekaanu <155834429+Efekaanu@users.noreply.github.com>
2024-02-11 20:26:20 +0300

    Add files via upload

---
+++ 2024Subat.py
@@ -0,0 +1,94 @@
+#a,b,c,d=50,100,50,75
+
+#sonuç=(a>b)
+#print(sonuç)
+
+#sonuç=(a==c)
+#print(sonuç)
+
+#sonuç=(a>d)
+#print(sonuç)
+
+#sonuç=(a!=c)
+#print(sonuç)
+
+#username1="Ceku"
+#username2="Arif"
+
+#sonuç=(username1=="Ahmet")
+#print("Eşleşme Sonucu:",sonuç)
+
+#Değişkenden bağımsız isim yazdık False çıktısını aldık
+
+#print("A.R.O.G.'a hoş geldiniz.\n")
+#username_input=input("Bir kullanıcı adı giriniz")
+
+#sonuç=(username1.lower()==username_input.ower())
+#sonuç2=(username2.lower()==username_input.lower())
+
+#sonuç3=(sonuç != sonuç2)
+
+#print("Eşleşme sonucu:",sonuç3)
+
+
+#fruits=['elma','muz','portakal','kivi']
+
+#print('kiraz' in fruits)
+
+#x=y=[10,20,30]
+#z=[10,20,30]
+
+#print(x is y)
+#print(x is z)
+#print(x == z)
+
+#vize=int(input("Vize notu gir"))
+#final=int(input("Final notu gir"))
+#print("Ortalaman",vize*0.4+final*0.6)
+
+#Ortalama bulur
+
+
+#girilen_sayı=int(input('sayı:'))
+#result=(girilen_sayı > 0) and (girilen_sayı %2==0)
+#print('Sonuç:' + str(result))
+
+#Sayı %2den büyükmü onu bulur ortalama
+
+
+#x=68
+#y=98
+#if (x > y):
+ #   print("En büyük sayı:",x)
+#    #<Eğer koşul doğru (True) ise çalışacak kodlar>
+#else:
+ #   print("En büyük sayı:",y)
+    #<Eğer koşul yanlış (False) ise çalışacak kodlar>
+
+
+#takım=input("sevdiğiniz takımın adını giriniz")
+#if(takım=="Galatasaray"):
+ #  print("En sevdiğiniz renkler:Sarı,Kırmızı")
+#elif(takım=="Fenerbahçe"):
+#     print("senin sevdiğin renk Lacivert,Sarı")
+#else(takım=="Kardemir Karabükspor"):
+  #   print("senin sevdiğin renk Kırmızı,Lacivert")
+
+print("HESAP MAKİNESİNE HOŞGELDİNİZ")
+"Toplama için-1"
+"Çıkarma için-2"
+"Çarpma için-3"
+"Bölme için-4"
+İşlem=input("Hangi işlemi tercih edersiniz?")
+a=int(input("1.sayıyı giriniz"))
+b=int(input("2. sayıyı giriniz"))
+
+if İşlem==1:
+    print(a+b)
+elif İşlem==2:
+    print(a-b)
+elif İşlem==3:
+    print(a*b)
+elif İşlem==4:
+    print(a/b)
+    
